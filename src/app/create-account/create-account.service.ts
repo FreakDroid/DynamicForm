3a63b91b71173b121da4b3ec3e1d41232b55e6b0
Freakdroid <wilfredo1285@gmail.com>
2019-04-23 07:18:30 -0300

    WIP Integrating logging.

--- src/app/create-account/create-account.service.ts
+++ src/app/create-account/create-account.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 // @ts-ignore
 import * as data from '../../assets/config.json';
+import {Token} from '../model/token';
 
 @Injectable({
   providedIn: 'root'
@@ -15,12 +16,18 @@ export class CreateAccountService {
     this.headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
   }
 
-  login(data) {
-    const body = new URLSearchParams();
-    body.set('X-Api-Token', data.xapitoken);
-    this.http.post(this.URL + 'token?', body, this.headers).subscribe(res => {
-      console.log(res);
-      return res;
-    });
+  async login(formValue) {
+    try {
+      const HEADERS = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+        .set('X-Api-Token', data.xapitoken);
+
+
+      const token = await this.http.get<Token>(this.URL + 'token', {headers: HEADERS}).toPromise();
+      console.log(token.data.token);
+
+      
+    } catch (e) {
+      throw e;
+    }
   }
 }
