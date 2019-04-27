ed620797912e8d9b9569914c5b5b0ca36bfe0ed7
Freakdroid <wilfredo1285@gmail.com>
2019-04-27 00:49:36 -0300

    WIP Integrating with backend. WIP.

--- src/app/create-account/create-account.service.ts
+++ src/app/create-account/create-account.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 // @ts-ignore
 import * as data from '../../assets/config.json';
-import {Token} from '../model/token';
+import {Token} from '../model/token.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,18 +16,25 @@ export class CreateAccountService {
     this.headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
   }
 
-  async login(formValue) {
-    try {
-      const HEADERS = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
-        .set('X-Api-Token', data.xapitoken);
+  register(formValue) {
+    const {userName, email, password, confirmPassword, firstName, middleName, lastName} = formValue;
+    console.log(userName, email, password, confirmPassword, firstName, middleName, lastName);
+    const HEADERS = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+      .set('X-Api-Token', data.xapitoken);
 
+    const payload = new HttpParams()
+      .set('email', email)
+      .set('username', userName)
+      .set('password', password)
+      .set('firstName', firstName)
+      .set('middleName', middleName)
+      .set('lastName', lastName)
+      .set('hearAboutFolionet', '1')
+      .set('investmentExperience', '1')
+      .set('investmentObjective', '1')
+      .set('password_confirmation', confirmPassword)
+      .set('_token', localStorage.getItem('token.model.ts'));
 
-      const token = await this.http.get<Token>(this.URL + 'token', {headers: HEADERS}).toPromise();
-      console.log(token.data.token);
-
-      
-    } catch (e) {
-      throw e;
-    }
+    return this.http.post(this.URL + 'user/register', payload, {headers: HEADERS});
   }
 }
--- src/app/login/login-service/login-service.service.ts
+++ src/app/login/login-service/login-service.service.ts
@@ -1,11 +1,9 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
-import {Token} from '../../model/token';
+import {Token} from '../../model/token.model';
 
 // @ts-ignore
 import * as data from '../../../assets/config.json';
-import {log} from 'util';
-import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +20,7 @@ export class LoginServiceService {
       const HEADERS = new HttpHeaders().set('X-Api-Token', data.xapitoken);
 
 
-      this.http.get<Token>(this.URL + 'token', {headers: HEADERS}).subscribe(res => {
+      this.http.get<Token>(this.URL + 'token.model.ts', {headers: HEADERS}).subscribe(res => {
           const token = res && res.data && res.data.token;
           console.log(token);
 
@@ -36,11 +34,11 @@ export class LoginServiceService {
 
           let body = `login=${login}&password=${password}&_token=${token}`;
 
-          this.http.post(this.URL + 'user/login', body, {headers: HEADERS}).subscribe(loginResponse => {
+          this.http.post(this.URL + 'user/login', payload, {headers: HEADERS}).subscribe(loginResponse => {
               console.log('loggin me');
               console.log(loginResponse);
               //If everything went ok
-              localStorage.setItem('token', token);
+              localStorage.setItem('token.model.ts', token);
               return true;
             },
             loginError => {
